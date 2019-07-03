a859017aa49b327a97b6fa6d125a2d69412f274d
Paweł Tuliński <pawel.tulinski@gmail.com>
2019-07-03 21:20:28 +0200

    Add files

---
+++ build/precache-manifest.b9d8977649e8fb3ace1571a46122b9d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23e0bece185f5baf01d0fa0ad4bed310",
+    "url": "/gamy-react/index.html"
+  },
+  {
+    "revision": "42dfd29fad85defc715c",
+    "url": "/gamy-react/static/css/main.0dde3f92.chunk.css"
+  },
+  {
+    "revision": "6912c74cd1a2efda3580",
+    "url": "/gamy-react/static/js/2.60abf8fa.chunk.js"
+  },
+  {
+    "revision": "42dfd29fad85defc715c",
+    "url": "/gamy-react/static/js/main.f57802a1.chunk.js"
+  },
+  {
+    "revision": "0576c6f6828134980d21",
+    "url": "/gamy-react/static/js/runtime~main.3d482a01.js"
+  },
+  {
+    "revision": "f83b41a2d28101dccde75e1fe4316ad2",
+    "url": "/gamy-react/static/media/kluczS.f83b41a2.png"
+  },
+  {
+    "revision": "af6fae3a5820c56936cff941f4941869",
+    "url": "/gamy-react/static/media/trumpetR+W.af6fae3a.png"
+  }
+]);
